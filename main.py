817e6bfaece8b0c415092ba647a37d602fa1fc98
ashutosh229 <akumarjha875@gmail.com>
2025-08-29 19:12:54 +0530

    pushed the websocket mechanism

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from routes import scraping
+from utils.scraper import scrapers_runner
+import asyncio
 
 load_dotenv()
 
 app = FastAPI()
 
+app.include_router(scraping.router)
 
+
+@app.on_event("startup")
+async def start_scraping():
+    async def scraper_loop():
+        while True:
+            scrapers_runner()
+            await asyncio.sleep(300)
+
+    asyncio.create_task(scraper_loop())
--- routes/scraping.py
+++ routes/scraping.py
@@ -5,11 +5,24 @@ clients: List[WebSocket] = []
 
 router = APIRouter()
 
+
 @router.websocket("/ws")
 async def websocket_entrypoint(websocket: WebSocket):
-    pass 
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            await websocket.receive_text()
+    except:
+        clients.remove(websocket)
+
 
 async def broadcast_new_article(article: dict):
+    to_remove = []
     for client in clients:
-        await client.send_json(article)
-        
+        try:
+            await client.send_json(article)
+        except:
+            to_remove.append(client)
+    for client in to_remove:
+        clients.remove(client)
